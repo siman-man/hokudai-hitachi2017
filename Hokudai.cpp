db68c8e4b908d573e71db8f9069f4be987166ad5
siman-man <tmshuichi@gmail.com>
2017-11-17 09:11:29 +0900

    160226

--- Hokudai.cpp
+++ Hokudai.cpp
@@ -184,7 +184,7 @@ public:
         double remainTime = 0.0;
         ll tryCount = 0;
         int R = 500000;
-        double k = 4.0;
+        double k = 4.5;
         int DD[8] = {-N-1, -N, -N+1, -1, 1, N-1, N, N+1};
 
         int currentScore = bestScore;
