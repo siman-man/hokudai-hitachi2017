d3a078cd9743ec91005d9eff6188575c026b3370
siman-man <tmshuichi@gmail.com>
2017-11-28 19:56:15 +0900

    163739

--- Hokudai.cpp
+++ Hokudai.cpp
@@ -185,7 +185,7 @@ public:
         int DD[8] = {-N - 1, -N, -N + 1, -1, 1, N - 1, N, N + 1};
 
         int currentScore = bestScore;
-        int thread = 30;
+        int thread = 25;
         int es = edgeList.size();
         double expCache[thread];
 
